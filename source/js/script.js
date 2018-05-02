38c900e038d6d21032bb333ebfb7ea13352566ba
Андрей <089ax@mail.ru>
2018-05-02 17:14:53 +0300

    Модалка со скриптом

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 var nav = document.querySelectorAll(".site-list");
 var buttontoggle = document.querySelector(".page-header__button");
+var buttonopen = document.querySelector(".products-item__link");
+var modalwindow = document.querySelector(".modal");
+var modalclose = document.querySelector(".modal__button");
 
 for (var i = 0; i < nav.length; i++){
   nav[i].classList.add("site-list--closed");
@@ -18,6 +21,25 @@ buttontoggle.addEventListener("click", function (evt) {
       nav[i].classList.add("site-list--closed");
       nav[i].classList.remove("site-list--opened");
       buttontoggle.classList.add("page-header__button--closed");
+    }
+  }
+});
+
+buttonopen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalwindow.classList.add("modal--opened");
+})
+
+modalclose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalwindow.classList.remove("modal--opened");
+})
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalwindow.classList.contains("modal--opened")) {
+      modalwindow.classList.remove("modal--opened");
     }
   }
 });
