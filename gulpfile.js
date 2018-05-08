cbd6f338e9f4f0ce9e27c89d2483464da688fca8
Андрей <089ax@mail.ru>
2018-05-08 23:43:11 +0300

    Сборщик

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,11 @@ var server = require("browser-sync").create();
 var svgstore = require("gulp-svgstore");
 var rename = require("gulp-rename");
 var webp = require("gulp-webp");
+var minifycss = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -17,13 +22,16 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minifycss())
+    .pipe(rename("style-min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +39,7 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
 gulp.task("sprite", function () {
@@ -47,4 +55,35 @@ gulp.task("webp", function () {
   return gulp.src("source/img/*.jpg")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/**/*.html"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml)
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    done
+  );
 });
