c1dfa9dfed719d7e73a7503bf8107b73324d76e1
Андрей <089ax@mail.ru>
2018-05-08 23:50:13 +0300

    gulp images

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,17 @@ gulp.task("serve", function() {
   gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
-gulp.task("sprite", function () {
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+      ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+  gulp.task("sprite", function () {
   return gulp.src("source/img/*.svg")
     .pipe(svgstore({
       inlineSvg: true
